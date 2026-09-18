8b1df7144a5702fceb0bb2af317e603ac2aa62e2
Meadow Liu <meadowsys@kiwin.gay>
2026-09-18 02:17:20 -0700

    support parsing duotone
    huh, we need really little info from these icons it seems, just the 2 d things from paths if its duotone and 1 otherwise, neat
    
    ~sf/vt

--- scripts/src/lib.rs
+++ scripts/src/lib.rs
@@ -1,7 +1,9 @@
 use roxmltree::Document as XmlDocument;
+use std::mem;
 
 pub struct Icon {
-	path_d: String
+	pub path_d: String,
+	pub path_d_duotone: Option<String>
 }
 
 impl Icon {
@@ -23,16 +25,36 @@ impl Icon {
 		assert!(root_children.next().is_none(), "must have only one root element");
 
 		let mut svg_children = svg.children();
-		let path = svg_children.next().unwrap();
+		let path = svg_children.next().expect("svg must have at least one child");
 		assert!(path.is_element(), "svg child must be element");
 		assert_eq!(path.tag_name().name(), "path", "svg child must be path tag");
-		assert_eq!(path.tag_name().namespace(), Some("http://www.w3.org/2000/svg"), "path should have svg xmlns");
-		assert_eq!(path.attributes().len(), 1, "extra attrs in path tag found");
-		let path_d = path.attribute("d").expect("path d attr must exist").into();
+		assert_eq!(path.tag_name().namespace(), Some("http://www.w3.org/2000/svg"), "svg child path should have svg xmlns");
+		let mut path_d = path.attribute("d").expect("path d attr must exist").into();
+		let mut path_d_duotone = None;
+		match path.attributes().len() {
+			1 => {
+				// other
+			}
+			2 => {
+				// duotone, need special handling
+				assert_eq!(path.attribute("opacity"), Some("0.2"), "first path in duotone icon must have opacity attr");
+				path_d_duotone = Some(mem::take(&mut path_d));
+				let path = svg_children.next().expect("svg must have two children in duotone icon");
+				assert!(path.is_element(), "svg child 2 must be element");
+				assert_eq!(path.tag_name().name(), "path", "svg child 2 must be path tag");
+				assert_eq!(path.tag_name().namespace(), Some("http://www.w3.org/2000/svg"), "svg child 2 path should have svg xmlns");
+				assert_eq!(path.attributes().len(), 1, "extra attrs in path tag found");
+				path_d = path.attribute("d").expect("path d attr must exist").into();
+			}
+			_ => {
+				// 0 can't happen, we just got an attribute before this match statement
+				panic!("extra attrs in path tag found");
+			}
+		}
 		assert!(svg_children.next().is_none(), "svg should have only one child");
 
 		assert!(!path.has_children(), "path should have no children");
 
-		Self { path_d }
+		Self { path_d, path_d_duotone }
 	}
 }
