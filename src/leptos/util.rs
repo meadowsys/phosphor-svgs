86364f879b67f1c239779b771846e1293e5b320c
Meadow Liu <meadowsys@kiwin.gay>
2026-09-20 11:31:16 -0700

    restructure a lot of the leptos code to make documentation nicer
    ~vt

--- src/leptos/util.rs
+++ src/leptos/util.rs
@@ -1,54 +1,13 @@
-use leptos::{ IntoView, component, view };
+use super::{ Bold, Duotone, Fill, Light, Regular, Thin };
+use leptos::{ IntoView, view };
 use leptos::attr::custom::CustomAttribute as _;
 use leptos::either::Either;
-use leptos::html::ElementChild as _;
 use leptos::reactive::wrappers::read::Signal;
-
-#[component]
-pub fn Icon<I, W>(
-	#[prop(optional)]
-	icon: I,
-	#[prop(optional)]
-	weight: W,
-	#[prop(into, default = Signal::stored(0.2))]
-	duotone_opacity: Signal<f32>
-) -> impl IntoView
-where
-	I: Default,
-	W: Default,
-	(I, W): GetIconData
-{
-	let _ = (icon, weight);
-
-	view! {
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor">
-			{<(I, W)>::get().into_paths(duotone_opacity)}
-		</svg>
-	}
-}
 
 pub struct IconPath {
 	pub(super) d: &'static str,
 	pub(super) has_opacity: bool
 }
-
-#[derive(Default)]
-pub struct Bold;
-
-#[derive(Default)]
-pub struct Duotone;
-
-#[derive(Default)]
-pub struct Fill;
-
-#[derive(Default)]
-pub struct Light;
-
-#[derive(Default)]
-pub struct Regular;
-
-#[derive(Default)]
-pub struct Thin;
 
 pub trait Icon {
 	fn bold_data() -> impl IconWeightData;
